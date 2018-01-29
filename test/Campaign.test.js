1f5de2223673f2f2b060b67a0db07406a1aecbfc
pllearns <pllearns@gmail.com>
2018-01-28 16:39:15 -0800

    finish campaign code for testing

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -28,5 +28,8 @@ beforeEach(async () => {
 
   [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
   
-  campaign = await new web3.eth.Contract(campaignAddress)
+  campaign = await new web3.eth.Contract(
+    JSON.parse(compiledCampaign.interface),
+    campaignAddress
+  );
 });
