fea7525d6a80af4c212a3efe7d8d998f25f67b90
pllearns <pllearns@gmail.com>
2018-01-30 20:02:29 -0800

    update card view for all campaign summary items

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -19,10 +19,57 @@ class CampaignShow extends Component {
     };
   }
 
+  renderCards () {
+
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount
+    } = this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: 'address of manager',
+        description: 'the creator of the campaign',
+        style: {overflowWrap: 'break-word'}
+      },
+      {
+        header: balance,
+        meta: 'balance of the campaign',
+        description: 'balance left for requests',
+        style: {overflowWrap: 'break-word'}
+      },
+      {
+        header: minimumContribution,
+        meta: 'minimum contribution',
+        description: 'how much to contribute to the campaign',
+        style: {overflowWrap: 'break-word'}
+      },
+      {
+        header: requestsCount,
+        meta: 'number of requests',
+        description: 'how many requests for this campaign',
+        style: {overflowWrap: 'break-word'}
+      },
+      {
+        header: approversCount,
+        meta: 'number of approvers',
+        description: 'number of contributors with approval power',
+        style: {overflowWrap: 'break-word'}
+      },
+    ]
+
+    return <Card.Group items={items} />
+  }
+
   render() {
     return (
       <Layout>
         <h3>Show page</h3>
+        {this.renderCards()}
       </Layout>
     )
   }
