07395497dc173fb155a86a7902ed61057c281034
pllearns <pllearns@gmail.com>
2018-01-30 18:33:25 -0800

    Update header to take in links to home and create pages

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes';
 
 export default () => {
   return (
     <Menu style={{ marginTop: '10px'}}>
-      <Menu.Item>
-        StartKickerCoin
-      </Menu.Item>
+      <Link route="/">
+        <a className="item">StartKickerCoin</a>
+      </Link>
       <Menu.Menu position="right">
         <Menu.Item>
           Campaigns
         </Menu.Item>
-        <Menu.Item>+</Menu.Item>
+        <Link route="/campaigns/new">
+          <a className="item">+</a>
+        </Link>
       </Menu.Menu>
     </Menu>
   );
