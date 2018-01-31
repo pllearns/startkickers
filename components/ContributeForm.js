e638f932ff2c73daac27dd7cfb05470620bb4787
pllearns <pllearns@gmail.com>
2018-01-31 14:34:18 -0800

    Add routes and pages for requests

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
 import { Form, Input, Message, Button } from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
+import web3 from '../ethereum/web3';
+import { Router } from '../routes';
 
 class ContributeForm extends Component {
 
   state = {
-    value: ''
+    value: '',
+    errMessage: '',
+    loading: false
   }
 
-  onSubmit = (event) => {
+  onSubmit = async event => {
     event.preventDefault();
+
+    this.setState({loading: true, errMessage: ''});
 
     const campaign = Campaign(this.props.address);
 
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, 'ether')
+      });
+      Router.replaceRoute(`/campaigns/${this.props.address}`)
+    } catch (err) {
+      this.setState({errMessage: err.message})
+    }
+    this.setState({loading: false, value: ''})
   }
 
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errMessage}>
         <Form.Field>
           <label>Contribution Amount</label>
           <Input
@@ -26,7 +43,8 @@ class ContributeForm extends Component {
             value={this.state.value}
             onChange={(event) => this.setState({value: event.target.value})}
           />
-          <Button primary>
+          <Message error header="Oh no!" content={this.state.errMessage} />
+          <Button loading={this.state.loading} primary>
             Contribute
           </Button>
         </Form.Field>
