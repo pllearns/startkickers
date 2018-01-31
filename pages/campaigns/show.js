bdd21a863e113aec4fd93696cd839bdfdece3d1d
pllearns <pllearns@gmail.com>
2018-01-30 19:04:18 -0800

    add campaign show component and update routing

---
+++ pages/campaigns/show.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CampaignShow extends Component {
+
+  render() {
+    return (
+      <h3>Show page</h3>
+    )
+  }
+}
+
+export default CampaignShow
