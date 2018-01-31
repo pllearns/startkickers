7c6c5f83e7b10ba3f88fe6d35605198a31cf04c1
pllearns <pllearns@gmail.com>
2018-01-30 17:35:33 -0800

    complete error handle

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,31 +1,35 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
   state = {
-    minimumContribution: ""
+    minimumContribution: '',
+    errMessage: ''
   };
 
   onSubmit = async (event) => {
     event.preventDefault();
 
-    const accounts = await web3.eth.getAccounts();
-
-    await factory.methods
-      .createCampaign(this.state.minimumContribution)
-      .send({
-        from: accounts[0]
-      });
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0]
+        });
+    } catch (err) {
+      this.setState({ errMessage: err.message });
+    }
   };
   
   render() {
   return (
     <Layout>
       <h3>Create Campaign</h3>
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errMessage}>
         <Form.Field>
           <label>Minimum Contribution</label>
           <Input 
@@ -36,6 +40,7 @@ class CampaignNew extends Component {
               this.setState({minimumContribution: event.target.value})}
           />
         </Form.Field>
+        <Message error header="Oh No!" content={this.state.errMessage}/>
         <Button primary>Create</Button>
       </Form>
     </Layout>
