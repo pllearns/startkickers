f6be0ed8128e558bf03e9408889b0f397955a19c
pllearns <pllearns@gmail.com>
2018-01-31 16:04:34 -0800

    add table for view of requests

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import Layout from '../../../components/Layout';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
+import Campaign from '../../../ethereum/campaign';
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
-    const { address } = props.query
+    const { address } = props.query;
+    const campaign = Campaign(address);
 
-    return {
-      address
-    }
+    const requestCount = await campaign.methods.getRequestsCount().call();
+
+    const requests = await Promise.all(
+      Array(parseInt(requestCount)).fill().map((element, index) => {
+        return campaign.methods.requests(index).call();
+      })
+    );
+    console.log(requests);
+
+    return { address, requests, requestCount }
   }
   render() {
+    const { Header, Row, HeaderCell, Body } = Table
     return (
       <Layout>
         <h3>Current Requests</h3>
@@ -22,6 +32,19 @@ class RequestIndex extends Component {
             </Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+        </Table>
       </Layout>
     )
   }
