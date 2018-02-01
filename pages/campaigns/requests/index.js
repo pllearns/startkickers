78be1bdc119ea9ba37b4300617b46e48e8250b40
pllearns <pllearns@gmail.com>
2018-01-31 16:18:46 -0800

    place cells with key info

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -3,6 +3,7 @@ import Layout from '../../../components/Layout';
 import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Campaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
@@ -20,6 +21,18 @@ class RequestIndex extends Component {
 
     return { address, requests, requestCount }
   }
+
+  renderRow() {
+    return this.props.requests.map((request, index) => {
+      return <RequestRow 
+        key={index}
+        id={index}
+        request={request}
+        address={this.props.address}
+      />;
+    });
+  }
+
   render() {
     const { Header, Row, HeaderCell, Body } = Table
     return (
@@ -44,6 +57,9 @@ class RequestIndex extends Component {
               <HeaderCell>Finalize</HeaderCell>
             </Row>
           </Header>
+          <Body>
+            {this.renderRow()}
+          </Body>
         </Table>
       </Layout>
     )
