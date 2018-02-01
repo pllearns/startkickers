f4777450f6b0c9011181be0ca35c0df6897e467a
pllearns <pllearns@gmail.com>
2018-01-31 16:54:39 -0800

    finalize function works

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -14,6 +14,15 @@ class RequestRow extends Component {
     })
   }
 
+  onFinalize = async () => {
+    const accounts = await web3.eth.getAccounts();
+    const campaign = Campaign(this.props.address);
+
+    await campaign.methods.finalizeRequest(this.props.id).send({
+      from: accounts[0]
+    })
+  }
+
   render() {
     const { Row, Cell } = Table;
     const { id, request, approversCount} = this.props;
@@ -25,7 +34,7 @@ class RequestRow extends Component {
         <Cell>{request.recipient}</Cell>
         <Cell>{request.approvalCount}/{approversCount}</Cell>
         <Cell><Button color="green" basic onClick={this.onApprove}>Approve</Button></Cell>
-        <Cell><Button color="red" basic>Reject</Button></Cell>
+        <Cell><Button color="teal" basic onClick={this.onFinalize}>Finalize</Button></Cell>
       </Row>
     )
   }
